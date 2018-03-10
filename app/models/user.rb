652910b4ec28c4693c22969e829bd79f7d70c3e3
gabrieelgomez <gagg1707@gmail.com>
2018-03-10 15:57:38 -0400

    commit

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  before_save :ensure_authentication_token
-  
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable
-end
