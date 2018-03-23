0314ec15716f15224fae7e4640de3c20eb7fadbb
gabrieelgomez <gagg1707@gmail.com>
2018-03-22 20:09:59 -0400

    Add middleware

--- config/application.rb
+++ config/application.rb
@@ -30,13 +30,24 @@ module BigwaveApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins '*'
+    #
+    #     resource '*',
+    #       headers: :any,
+    #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    #   end
+    # end
+
+
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-
         resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete, :put, :path, :head]
       end
     end
 
