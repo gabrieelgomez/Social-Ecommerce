c5abe2a32df974b84f3056202cc8e5a03723c907
enmachs <enmachs@gmail.com>
2018-03-23 11:27:42 -0400

    routing change x2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match '*path', :controller => 'application', :action => 'handle_options_request', :via => [:get, :post, :options]
+  #match '*path', :controller => 'application', :action => 'handle_options_request', :via => [:get, :post, :options]
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Route for frontend
