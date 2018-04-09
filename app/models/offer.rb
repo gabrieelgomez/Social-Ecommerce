f39b3d4ef0e4fbb87b1d170ac2715592a3c9e57e
enmachs <enmachs@gmail.com>
2018-04-09 13:09:08 -0400

    Offer module with geolocation integrated

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -7,4 +7,6 @@ class Offer < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude,
                       :address => :address
   after_validation :reverse_geocode  # auto-fetch address
+  # geocoded_by :address
+  # after_validation :geocode
 end
