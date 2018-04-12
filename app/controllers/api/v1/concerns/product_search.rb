a1984ffedb1ea6387d2bbb3e8b2fe6b71896bc48
enmachs <enmachs@gmail.com>
2018-04-12 11:59:12 -0400

    Product search and modulation

--- app/controllers/api/v1/concerns/product_search.rb
+++ app/controllers/api/v1/concerns/product_search.rb
@@ -1,32 +1,33 @@
-module Api::V1::Concerns::ProductSearch
-	extend ActiveSupport::Concern
+module Api::V1::Concerns
 	include ModelModulation
-	private
+	module ProductSearch
+		extend ActiveSupport::Concern
+		private
 
-	def product_params
-		params.require(:product).permit(:name, :images, :category_ids, :height, :width,
-                                    :weight, :price, { product_relations: [] }, :tags, :rate)
+		def product_params
+			params.require(:product).permit(:name, :images, :category_ids, :height, :width,
+	                                    :weight, :price, { product_relations: [] }, :tags, :rate)
+		end
+
+	  def set_product
+	  	@product = @productable.products.find(params[:id])
+	  end
+
+	  # def model_name
+	  #   params[:type_profile].singularize.camelize.constantize
+	  # end
+
+	  # to show and index actions
+	  # def public_productable
+	  # 	@productable = model_name.find_by_id_and_type_profile(params[:profile_id],
+	  #                                                         params[:type_profile].singularize)
+	  # end
+
+	  # to create, destroy and update
+	  # def current_user_productable
+	  # 	@productable = model_name.find_by_id_and_type_profile_and_user_id(params[:profile_id],
+	  #                                                         						params[:type_profile].singularize,
+	  #                                                         						current_v1_user.id)
+	  # end
 	end
-
-  def set_product
-  	@product = @productable.products.find(params[:id])
-  end
-
-  # def model_name
-  #   params[:type_profile].singularize.camelize.constantize
-  # end
-
-  # to show and index actions
-  # def public_productable
-  # 	@productable = model_name.find_by_id_and_type_profile(params[:profile_id],
-  #                                                         params[:type_profile].singularize)
-  # end
-
-  # to create, destroy and update
-  # def current_user_productable
-  # 	@productable = model_name.find_by_id_and_type_profile_and_user_id(params[:profile_id],
-  #                                                         						params[:type_profile].singularize,
-  #                                                         						current_v1_user.id)
-  # end
-
 end
