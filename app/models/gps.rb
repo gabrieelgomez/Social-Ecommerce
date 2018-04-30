32def1779ad8f2f66bdea9bc136d82df091e0cdc
enmachs <enmachs@gmail.com>
2018-04-30 17:16:53 -0400

    added searching by params :q

--- app/models/gps.rb
+++ app/models/gps.rb
@@ -1,21 +1,27 @@
 class Gps
-  def self.categ_search(params)
-    cats = params[:categories].params_to_a
-    profiles = cats.collect { |n| Category.try(:find, n).profiles }
-    profiles.pop
-    radio = params[:radio] || 2
-    # byebug
-    selected_prof = profiles.select do |prof|
-      Geocoder::Calculations.distance_between(params[:user], [lat(prof), long(prof)]) < radio.to_i
-      # puts Geocoder::Calculations.distance_between(params[:user], [prof.locations.first.latitude, prof.locations.first.longitude])
-    end
-  end
-
-  def lat(prof)
+  def self.latitud(prof)
     prof.locations.first.latitude
   end
 
-  def long(prof)
+  def self.longitud(prof)
     prof.locations.first.longitude
   end
+
+  def self.categ_search(params)
+    cats = params[:categories].params_to_a
+    profiles = cats.collect { |n| Category.try(:find, n).profiles }.first
+    # profiles.pop
+    radio = params[:radio] || 2
+    q = params[:q].downcase || ''
+    # byebug
+    selected_prof = profiles.select do |prof|
+      # Geocoder::Calculations.distance_between(params[:user], [latitud(prof), longitud(prof)])
+      # byebug
+      Geocoder::Calculations.distance_between(params[:user], [latitud(prof), longitud(prof)]) < radio.to_i && (prof.name.downcase.include?(q) || prof.title.downcase.include?(q))
+      # puts Geocoder::Calculations.distance_between(params[:user], [prof.locations.first.latitude, prof.locations.first.longitude])
+    end
+    # byebug
+
+
+  end  
 end
