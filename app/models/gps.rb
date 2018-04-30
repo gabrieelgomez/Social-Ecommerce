bbe6e455fb53c3758969f6ec697c03e0d5d772c8
enmachs <enmachs@gmail.com>
2018-04-30 10:48:46 -0400

    gps  methods

--- app/models/gps.rb
+++ app/models/gps.rb
@@ -1,12 +1,21 @@
 class Gps
   def self.categ_search(params)
     cats = params[:categories].params_to_a
-    profiles = cats.collect { |n| Category.try(:find, n).profiles }.first
+    profiles = cats.collect { |n| Category.try(:find, n).profiles }
+    profiles.pop
     radio = params[:radio] || 2
-    byebug
+    # byebug
     selected_prof = profiles.select do |prof|
-      Geocoder::Calculations.distance_between(params[:user], [prof.locations.first.latitude, prof.locations.first.longitude]) < radio.to_i
+      Geocoder::Calculations.distance_between(params[:user], [lat(prof), long(prof)]) < radio.to_i
       # puts Geocoder::Calculations.distance_between(params[:user], [prof.locations.first.latitude, prof.locations.first.longitude])
     end
+  end
+
+  def lat(prof)
+    prof.locations.first.latitude
+  end
+
+  def long(prof)
+    prof.locations.first.longitude
   end
 end
