3fe0a8c68e040cca1391ab55f12b401d9337ed8c
enmachs <enmachs@gmail.com>
2018-05-01 22:43:49 -0400

    increased logic in gps model

--- app/models/gps.rb
+++ app/models/gps.rb
@@ -1,19 +1,36 @@
 class Gps
   def self.latitud(prof)
-    prof.locations.first.latitude
+    prof.locations.first.try(:latitude)
   end
 
   def self.longitud(prof)
-    prof.locations.first.longitude
+    prof.locations.first.try(:longitude)
+  end
+
+  def self.get_distance(user, prof)
+    Geocoder::Calculations.distance_between(user,
+                                            [latitud(prof), longitud(prof)])
+  end
+
+  def self.eval_q(prof, q)
+    prof.name.downcase.include?(q) || prof.title.downcase.include?(q)
+  end
+
+  def self.get_profiles(categories)
+    return Profile.all if categories.nil?
+    categories.collect { |n| Category.try(:find, n).profiles }.first
   end
 
   def self.categ_search(params)
-    cats = params[:categories].params_to_a
-    profiles = cats.collect { |n| Category.try(:find, n).profiles }.first
-    radio = params[:radio] || 2
+    cats = params[:categories].try(:map, &:to_i)
+    radio = params[:radio] || 0
     q = params[:q].downcase || ''
-    selected_prof = profiles.select do |prof|
-      Geocoder::Calculations.distance_between(params[:user], [latitud(prof), longitud(prof)]) < radio.to_i && (prof.name.downcase.include?(q) || prof.title.downcase.include?(q))
+    get_profiles(cats).select do |prof|
+      if radio.zero?
+        eval_q(prof, q)
+      else
+        get_distance(params[:user], prof) < radio.to_i && eval_q(prof, q)
+      end
     end
-  end  
+  end
 end
