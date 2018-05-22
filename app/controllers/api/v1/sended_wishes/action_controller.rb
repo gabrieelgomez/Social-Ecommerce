22fe5a4d62d41e7c0a073f0d1a5eb5125745abda
Enmanuel <enmachs@gmail.com>
2018-05-22 15:16:20 -0400

    routes of sended wishes

--- app/controllers/api/v1/sended_wishes/action_controller.rb
+++ app/controllers/api/v1/sended_wishes/action_controller.rb
@@ -1,7 +1,13 @@
 module Api::V1::SendedWishes
   class ActionController < SendedWishesController
 
+    before_action :set_user
+    before_action :set_wish
+    before_action :set_profile
+
     def send_wish
+      byebug
+      @sended_wish = SendedWish.new(sended_wish_params)
     end
   end
 end
--- app/models/sended_wish.rb
+++ app/models/sended_wish.rb
@@ -1,5 +1,9 @@
 class SendedWish < ApplicationRecord
+  # Relations
   belongs_to :user
   belongs_to :profile
   belongs_to :wish
+
+  # Validations
+  validates :profile_id, :user_id, :wish_id, presence: true
 end
