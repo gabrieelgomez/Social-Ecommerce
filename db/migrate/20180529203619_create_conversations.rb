55f71e1d1e8474502af88c3d91ab9cb9ce0b1255
Enmanuel <enmachs@gmail.com>
2018-05-29 16:53:27 -0400

    adding index to conversation migration

--- db/migrate/20180529203619_create_conversations.rb
+++ db/migrate/20180529203619_create_conversations.rb
@@ -1,10 +1,12 @@
 class CreateConversations < ActiveRecord::Migration[5.1]
   def change
     create_table :conversations do |t|
-      t.references :senderable, polymorhic: true, foreign_key: true
+      t.references :senderable, polymorhic: true
       t.references :recipientable, polymorphic: true
-
+      
       t.timestamps
     end
+    add_index :conversations, %i[recipientable_id recipientable_type,
+                                 senderable_id senderable_type], unique: true
   end
 end
