502c61b230e9142001acec3952963971b25da4fa
gabrieelgomez <gagg1707@gmail.com>
2018-06-06 18:46:59 -0400

    merge issue

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < NotificationHandler::Notification
 
   def self.create_notify_models(recipient, sender, type, message)
-    notification = recipient.notify type: type, metadata: {
+    recipient.notify metadata: {
       title: message
     }
     # NotificationMailer.push(message, to: recipient.email).deliver_now
