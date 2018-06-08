02f5d418cc622a76cc3dd328ff14f27a3d3a1386
Enmanuel <enmachs@gmail.com>
2018-06-08 12:21:06 -0400

    description added description field to products

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :height, :weight, :files, :width, :images,
              :price, :stock, :status, :num_ref, :bar_code, :currency,
-             :price_ranges, :tags, :custom_fields,
+             :price_ranges, :tags, :custom_fields, :description,
              :options, :product_relations, :productable
   # belongs_to :productable
 end
