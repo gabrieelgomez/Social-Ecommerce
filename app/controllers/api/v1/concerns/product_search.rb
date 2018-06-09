937bd153072606b1fffb5650a8fdb1f53d67c705
gabrieelgomez <gagg1707@gmail.com>
2018-06-09 10:35:57 -0400

    fix concerns products

--- app/controllers/api/v1/concerns/product_search.rb
+++ app/controllers/api/v1/concerns/product_search.rb
@@ -4,11 +4,11 @@ module Api::V1::Concerns::ProductSearch
   private
 
   def product_params
-    params.require(:product).permit(:name, :images, :category_ids, :height, :files
-                                    :fields, :prominent, :virtual_product
+    params.require(:product).permit(:name, :images, :category_ids, :height, :files,
+                                    :fields, :prominent, :virtual_product,
                                     :width, :weight, :price, { product_relations: [] },
                                     :tags, :rate, :stock, :num_ref, :status, :bar_code,
-                                    :brand, :currency, :service_type, :description, :)
+                                    :brand, :currency, :service_type, :description, )
   end
 
   def set_product
