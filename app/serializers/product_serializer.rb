e32498781d0d342c6af610e3c33ce445355867bd
gabrieelgomez <gagg1707@gmail.com>
2018-06-13 18:44:30 -0400

    fix serializer product with field subcategories only

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,6 +2,6 @@ class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :height, :weight, :rate, :files, :width, :images,
              :stock, :status, :num_ref, :bar_code, :currency, :description, :fields,
              :virtual_product, :price_ranges, :tags, :custom_fields, :prominent,
-             :brand, :service_type, :options, :build_products_relations, :productable
+             :brand, :service_type, :options, :subcategories, :build_products_relations, :productable
   # belongs_to :productable
 end
