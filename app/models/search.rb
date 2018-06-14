bb5327d210c9d43ea061f5d61c89a152d69fa4b3
Enmanuel <enmachs@gmail.com>
2018-06-13 20:45:45 -0400

    added logic and some fixes to general search

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,13 +8,10 @@ class Search
     # profiles_by_q = filter_by_q(search)
     profiles_by_cat = filter_by_catgs(categories,
                                       search).to_a
-    # byebug
-    # profiles = (profiles_by_cat)
-    # byebug
 
     # Filtering products
     products_q = products_by_q(search)
-    products_cat = product_by_subcat(categories)
+    products_cat = product_by_subcat(categories).flatten
     products = (products_q + products_cat).uniq
 
     (profiles_by_cat + products).flatten
@@ -22,37 +19,15 @@ class Search
 
   # Filter profiles by categories
   def self.filter_by_catgs(categories, search)
-    # profiles =
-    # result = []
-    # categories.map{|cat| profiles}
-    # search_result = categories.collect do |cat|
-    #   Profile.ransack(categories_id_eq: cat).result
-    #          .ransack(name_or_description_or_title_cont: search)
-    # end
-    # Profile.ransack(categories_id_m)
-    Pyme.ransack(categories_id_in: categories).result
-        .ransack(name_or_description_or_title_cont: search).result
-    # byebug0
-    # search_result.map{|r| r.result}.flatten.uniq
-    # profiles.select do |profile|
-    #   categories.include?(profile.categories.map(&:id))
-    # end
+    Pyme.short_ransack(categories, search) +
+      Independent.short_ransack(categories, search) +
+      Seller.short_ransack(categories, search)
   end
-
-  # # Filter profiles by :q param
-  # def self.filter_by_q(search)
-  #   return Profile.all if search.empty?
-  #   Profile.ransack(name_or_description_or_title_cont: search)
-  #          .result
-  #          .to_a
-  #   # byebug
-  # end
 
   # Filter products by :q param
   def self.products_by_q(search)
     return [] if search.blank?
-    # byebug
-    Product.ransack(name_cont: search).result.to_a
+    Product.ransack(name_cont: search).result
   end
 
   # Filter products by categories/subcategories
