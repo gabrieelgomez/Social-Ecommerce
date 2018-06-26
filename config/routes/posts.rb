9d758cbbf6941bceddad229142371bab4473f638
Enmanuel <enmachs@gmail.com>
2018-06-26 16:29:36 -0400

    crud of post done

--- config/routes/posts.rb
+++ config/routes/posts.rb
@@ -3,4 +3,5 @@ namespace :posts do
   get '/', to: 'show#index'
   get '/:post_id', to: 'show#show'
   delete '/:post_id/destroy', to: 'destroy#destroy'
+  put '/:post_id/update', to: 'update#update'
 end
