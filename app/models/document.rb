2ef6e6a026fad86700e88e285014c9a31dfddb44
gabrieelgomez <gagg1707@gmail.com>
2018-06-29 10:44:18 -0400

    change base63 documents, before uploader_since, request frontend

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ApplicationRecord
-  mount_uploader :document, DocumentUploader
+  mount_base64_uploader :document, DocumentUploader
   # belongs_to :product
   belongs_to :documentable, polymorphic: true
   validates :document, presence: true
