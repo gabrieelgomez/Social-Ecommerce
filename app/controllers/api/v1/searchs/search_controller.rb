7e9367aca1e98c654a33c774befd649ba3540149
gabrieelgomez <gagg1707@gmail.com>
2018-07-19 11:18:31 -0400

    change search filters

--- app/controllers/api/v1/searchs/search_controller.rb
+++ app/controllers/api/v1/searchs/search_controller.rb
@@ -3,11 +3,17 @@ module Api::V1
 
     def global_search
       @result = Search.deep_search(params)
+      render json: @result, status: 200
+    end
+
+    def filters_search
+      @result = LookFor.search(params)
       render json: {
           products: @result.first,
           profiles: @result.second,
           filters: @result.third
         }, status: 200
     end
+
   end
 end
