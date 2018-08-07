e42068fdbb24fd747dbd91017580afe03b416bdf
Enmanuel <enmachs@gmail.com>
2018-08-07 01:15:50 -0400

    Uncommented out categories and subcategories actions

--- app/controllers/api/v1/products/subcategories/actions_controller.rb
+++ app/controllers/api/v1/products/subcategories/actions_controller.rb
@@ -1,34 +1,32 @@
 module Api::V1::Products::Subcategories
   class ActionsController < SubcategoriesController
     before_action :set_subcategory, only: [:update, :destroy]
-
     def create
-      # @subcategories = Subcategory.new(subcategories_params)
-      # if @subcategories.save
-      #   render json: @subcategories, status: 200
-      # else
-      #   render json: @subcategories.errors,
-      #          status: 500
-      # end
+      @subcategories = Subcategory.new(subcategories_params)
+      if @subcategories.save
+        render json: @subcategories, status: 200
+      else
+        render json: @subcategories.errors,
+               status: 500
+      end
     end
 
     def update
-      # if @subcategory.update(subcategories_params)
-      #   render json: @subcategory, status: 200
-      # else
-      #   render json: @subcategory.errors,
-      #          status: 500
-      # end
+      if @subcategory.update(subcategories_params)
+        render json: @subcategory, status: 200
+      else
+        render json: @subcategory.errors,
+               status: 500
+      end
     end
 
     def destroy
-      # if @subcategory.destroy
-      #   render json: @subcategory, status: 200
-      # else
-      #   render json: @subcategory.errors,
-      #          status: 500
-      # end
+      if @subcategory.destroy
+        render json: @subcategory, status: 200
+      else
+        render json: @subcategory.errors,
+               status: 500
+      end
     end
-
   end
 end
--- app/controllers/api/v1/profiles/categories/actions_controller.rb
+++ app/controllers/api/v1/profiles/categories/actions_controller.rb
@@ -1,30 +1,28 @@
 module Api::V1::Profiles::Categories
-	class ActionsController < CategoriesController
+  class ActionsController < CategoriesController
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        render json: @category
+      else
+        render json: ErrorSerializer.serialize(@category.errors)
+      end
+    end
 
-    # def create
-    #   @category = Category.new(category_params)
-    #   if @category.save
-    #     render json: @category
-    #   else
-    #     render json: ErrorSerializer.serialize(@category.errors)
-    #   end
-    # end
-    #
-    # def update
-		# 	if @category.update(category_params)
-    #     render json: @category, status: 200
-    #   else
-    #     render json: ErrorSerializer.serialize(@category.errors)
-    #   end
-		# end
-    #
-		# def destroy
-    # 	if @category.destroy
-    #     render json: @category, status: 200
-    #   else
-    #     render json: ErrorSerializer.serialize(@category.errors)
-    #   end
-		# end
+    def update
+      if @category.update(category_params)
+        render json: @category, status: 200
+      else
+        render json: ErrorSerializer.serialize(@category.errors)
+      end
+    end
 
+    def destroy
+      if @category.destroy
+        render json: @category, status: 200
+      else
+        render json: ErrorSerializer.serialize(@category.errors)
+      end
+    end
   end
 end
