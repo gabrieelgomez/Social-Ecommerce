f47152155eb40189c82a58682493ffe75e9c218e
gabrieelgomez <gagg1707@gmail.com>
2018-08-08 19:28:12 -0400

    change all profiles

--- app/controllers/api/v1/profiles/profiles_controller.rb
+++ app/controllers/api/v1/profiles/profiles_controller.rb
@@ -2,6 +2,11 @@ module Api::V1
   class Profiles::ProfilesController < ApiController
     include ::Api::V1::Concerns::ModelModulation
 
+    def all_profiles
+      @result = Profile.all
+      render json: @result, status: 200
+    end
+
     private
     def set_profile
       @profile = custom_find {
@@ -13,11 +18,6 @@ module Api::V1
       profile = Profile.find(params[:id]) rescue Profile
       permission = current_v1_user.has_role? [:admin, :editor], profile
       @profile = profile if permission
-    end
-
-    def all_profiles
-      @result = Profile.all
-      render json: @result, status: 200
     end
 
   end
