bb63e8b046a2b6945067afde3ba701e5256abdfd
gabrieelgomez <gagg1707@gmail.com>
2018-08-14 15:31:19 -0400

    fix birthdate

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :nickname, :avatar, :url,
              :created_at, :updated_at, :offers, :censured, :slug,
-             :dni, :gender, :birth_day, :country
+             :dni, :gender, :birth_date, :country
 end
