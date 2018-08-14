fe13d165eb3c89b771a8ed88fb89ae0b76de1a4d
Enmanuel <enmachs@gmail.com>
2018-08-14 12:05:39 -0400

    added created_at to response of wish

--- app/serializers/wish_serializer.rb
+++ app/serializers/wish_serializer.rb
@@ -1,4 +1,7 @@
 class WishSerializer < ActiveModel::Serializer
   attributes :id, :name, :budget, :description, :user,
-             :wisheable, :priority
+             :wisheable, :priority, :created_time
+  def created_time
+    object.created_at.strftime("%F / %T")
+  end
 end
