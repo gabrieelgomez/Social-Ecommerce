df794a883cdea4813e69e02dce13f79d0bdf0f5c
gabrieelgomez <gagg1707@gmail.com>
2018-08-14 15:28:41 -0400

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :nickname, :avatar, :url,
-             :created_at, :updated_at, :offers, :censured, :slug
+             :created_at, :updated_at, :offers, :censured, :slug,
+             :dni, :gender, :birth_day, :country
 end
