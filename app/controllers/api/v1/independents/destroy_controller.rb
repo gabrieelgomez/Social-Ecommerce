f32680ffc3ddadac0fb607835ea67cd506a40e3e
gabrieelgomez <gagg1707@gmail.com>
2018-08-14 13:54:32 -0400

    fix destroy independent

--- app/controllers/api/v1/independents/destroy_controller.rb
+++ app/controllers/api/v1/independents/destroy_controller.rb
@@ -4,10 +4,10 @@ module Api::V1::Independents
   class DestroyController < IndependentsController
     before_action :authenticate_v1_user!, only: %i[destroy]
     before_action :validate_password, only: %i[destroy]
-    before_action :set_my_independent, only: %i[update]
+    before_action :set_my_independent, only: %i[destroy]
 
     def destroy
-      return access_denied if current_v1_user.has_role? [:editor, :moderator], @pyme
+      return access_denied if current_v1_user.has_role? [:editor, :moderator], @independent
       if @independent.destroy
         render json: @independent, status: 200
       else
