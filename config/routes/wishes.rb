c4807f60f0c1602f8706468c81a9dd1859eb95b7
Enmanuel <enmachs@gmail.com>
2018-08-16 21:00:07 -0400

    destroy of wish asking for current_password - done

--- config/routes/wishes.rb
+++ config/routes/wishes.rb
@@ -2,7 +2,7 @@
 namespace :wishes do
   get '/my_wishes', to: 'show#my_wishes'
   post '/create', to: 'create#create'
-  delete '/:wish_id/destroy', to: 'destroy#destroy'
+  put '/:wish_id/destroy', to: 'destroy#destroy'
   put '/:wish_id/update', to: 'update#update'
 end
 # --- Wishes routes - end
