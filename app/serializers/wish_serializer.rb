95faa68e6a67a48f70ec7b76072e5e5c67134413
Enmanuel <enmachs@gmail.com>
2018-08-16 20:54:03 -0400

    url of products and profile done - wish serializer

--- app/serializers/wish_serializer.rb
+++ app/serializers/wish_serializer.rb
@@ -1,6 +1,16 @@
 class WishSerializer < ActiveModel::Serializer
   attributes :id, :name, :budget, :description, :user,
              :wisheable, :priority, :created_time
+
+  def wisheable
+    wisheable = object.wisheable
+    object.wisheable.as_json.merge({
+      productable_url: wisheable.productable.url,
+      wisheable_url: wisheable.productable.url + "/products/#{wisheable.id}"
+    })
+    # /v1/:type_profile/:profile_id/products/:id
+  end
+
   def created_time
     object.created_at.strftime("%F / %T")
   end
