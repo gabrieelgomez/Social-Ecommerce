f04279e346fdb6ed89cf470e37cdeab54ae0ed42
Enmanuel <enmachs@gmail.com>
2018-08-31 16:48:32 -0400

    included subcategories in categories

--- app/serializers/independent_serializer.rb
+++ app/serializers/independent_serializer.rb
@@ -1,3 +1,17 @@
 class IndependentSerializer < ProfileSerializer
   attributes :products
+
+  def products
+    object.products.map do |product|
+      product.as_json(
+        include: [
+          'subcategories' => {
+            only: [:id, :name]
+          }
+        ]
+      ).merge(
+        categories: product.productable.categories
+      )
+    end
+  end
 end
