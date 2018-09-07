8f26879649f156b702390b999a595fd4fe0e69eb
Enmanuel <enmachs@gmail.com>
2018-09-06 20:40:47 -0400

    validations to model contact and contactType done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
   belongs_to :contact_type
   belongs_to :profile
+
+  validates :name, :email, :comments, presence: true
 end
--- app/models/contact_type.rb
+++ app/models/contact_type.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# Contact Type Model
 class ContactType < ApplicationRecord
+  validates :name, presence: true
 end
