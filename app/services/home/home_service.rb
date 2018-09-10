733ccfbe0a01ee31978beee1a19d5bcd3fdb22d3
Gabriel Gómez <gagg1707@gmail.com>
2018-09-10 15:38:35 -0400

    mejoras de metodos

--- app/services/home/home_service.rb
+++ app/services/home/home_service.rb
@@ -4,7 +4,7 @@ module Home
     # Services #2 independents_by_created_at, Enmachs
     def self.independents(params)
       quantity = params[:quantity] || 10
-      Independent.order('created_at DESC')
+      Independent.order(created_at: :desc)
                  .first(quantity)
                  .as_json(only: %i[id title photo name type_profile])
     end
@@ -21,10 +21,9 @@ module Home
     # Services #5 products_by_created_at, Enmachs
     def self.products_by_created_at(params)
       quantity = params[:quantity] || 10
-      Product.order('created_at DESC')
+      Product.order(created_at: :desc)
              .first(quantity)
              .as_json(only: %i[id name])
-      # byebug
     end
 
     # Services #6 products_by_wishes, Enmachs
@@ -34,14 +33,15 @@ module Home
              .reverse
              .first(quantity)
              .as_json(only: %i[id name])
-      # byebug
     end
 
     # --------------------------------------------------------------------------
 
     # Services #1 profiles_by_created_products, GabrielGomez
     def self.profiles
-      Pyme.all.sort_by{ |profile| (profile.products.map(&:status)-[false]).count }.reverse.as_json(only: [:id, :title, :photo, :created_at, :updated_at, :type_profile], methods: :category_ids)
+      Pyme.all.sort_by{ |profile| (profile.products.map(&:status)-[false]).count }
+              .reverse
+              .as_json(only: %i[id title photo created_at updated_at type_profile], methods: :category_ids)
     end
 
     # Services #4 categories_listing_by_products, GabrielGomez
@@ -51,22 +51,17 @@ module Home
 
     # Services #7 categories_by_profiles_nested_products, GabrielGomez
     def self.categories_by_profiles_nested_products
-      # Response de categorias con profiles que tengan mas productos de esa categoria
       categories =  Category.all.collect{ |category|
         {
           category_id: category.id,
           category_name: category.name,
-          profiles: category.by_profiles.sort_by{|profile| profile.products.select{|product| product.category_ids.include?(category.id) }.count}.reverse.take(6).as_json(only: [:id, :title, :photo, :created_at, :updated_at, :type_profile], methods: :category_ids)
-          # profiles: category.profiles_products_active
+          profiles: category.by_sorting_profiles_products
+                            .reverse
+                            .take(6)
+                            .as_json(only: %i[id title photo created_at updated_at type_profile], methods: :category_ids)
         }
       }.uniq
-      categories
     end
 
-    # private
-
-    # def quantity
-    #   params[:quantity] || 10
-    # end
   end
 end
