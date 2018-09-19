c2a28ed1992ab66ac1ac3aaccc19c4fede266635
Gabriel Gómez <gagg1707@gmail.com>
2018-09-19 19:53:23 -0400

    fix locatable show locations controller

--- app/controllers/api/v1/locations/show_controller.rb
+++ app/controllers/api/v1/locations/show_controller.rb
@@ -1,16 +1,17 @@
 module Api::V1::Locations
   class ShowController < LocationsController
     # before_action :authenticate_v1_user!
-    before_action :set_locatable, only: %i[show]
+    # before_action :set_locatable, only: %i[show]
 
     def index
       @locations = Location.all
-      render json: @locations, status: 200 
+      render json: @locations, status: 200
     end
 
     def show
+      @locatable_type = params[:locatable_type].modelarize
+      @locatable = @locatable_type.find(params[:locatable_id])
       @locations = @locatable.locations
-      byebug
       render json: @locations, status: 200
     end
   end
