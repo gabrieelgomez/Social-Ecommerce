57d68c44e13b31c4ba603ee22bb60f4491f8f2e1
gabrieelgomez <gagg1707@gmail.com>
2018-12-13 15:50:50 -0400

    change current_v1_user to current_user

--- app/channels/notification_channel.rb
+++ app/channels/notification_channel.rb
@@ -1,7 +1,7 @@
 class NotificationChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from "notifications-#{current_v1_user.id}"
+    stream_from "notifications-#{current_user.id}"
   end
 
   def unsubscribed
