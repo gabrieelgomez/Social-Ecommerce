50065fcba1ba0bcd305a695c0ee779dcfbdffece
gabrielgomez <gagg1707@gmail.com>
2019-02-24 20:28:55 -0400

    subategories sort by products charged

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -8,4 +8,8 @@ class Subcategory < ApplicationRecord
     Arel.sql("unaccent(\"name\")")
   end
 
+  def products_charged
+    products.count
+  end
+
 end
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,10 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :cover, :name, :subcategories
+
+  def subcategories
+    object.subcategories.select{|subcategory|
+      subcategory.products.count.positive?
+    }.as_json(only: %i[id name category_id], methods: %i[products_charged])
+  end
+
 end
