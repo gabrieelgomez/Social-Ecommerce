cd08250579fb29bfee4a116069cb8d7f11dd5866
gabrielgomez <gagg1707@gmail.com>
2019-03-06 08:41:56 -0400

    countri code downcase

--- db/migrate/20180429135537_create_locations.rb
+++ db/migrate/20180429135537_create_locations.rb
@@ -1,6 +1,7 @@
 class CreateLocations < ActiveRecord::Migration[5.1]
   def change
     create_table :locations do |t|
+      t.string :title
       t.text   :address
       t.float  :latitude
       t.float  :longitude
@@ -8,6 +9,7 @@ class CreateLocations < ActiveRecord::Migration[5.1]
       t.string :state_code
       t.string :country
       t.string :country_code
+      t.jsonb  :metadata
       t.references :locatable, polymorphic: true
 
       t.timestamps
