6455422fa5cf476716eefb90b27f0f68c7300765
gabrielgomez <gagg1707@gmail.com>
2019-04-01 15:14:22 -0400

    add foregn key

--- db/migrate/20190401174638_add_column_conversation_id_to_cotizations.rb
+++ db/migrate/20190401174638_add_column_conversation_id_to_cotizations.rb
@@ -1,5 +1,5 @@
 class AddColumnConversationIdToCotizations < ActiveRecord::Migration[5.1]
   def change
-    add_reference :cotizations, :conversation, foreign_key: true
+    add_reference :cotizations, :conversation#, foreign_key: true
   end
 end
