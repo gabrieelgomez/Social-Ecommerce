acb21cab48c342df2f656b6ea5c37cf7e9e567b7
gabrielgomez <gagg1707@gmail.com>
2019-04-23 12:58:22 -0400

    filter categories wishes

--- app/controllers/api/v1/wishes/show_controller.rb
+++ app/controllers/api/v1/wishes/show_controller.rb
@@ -3,8 +3,19 @@ module Api::V1::Wishes
     before_action :authenticate_v1_user!, except: [:wishes_by_user]
 
     def my_wishes
-      @user = current_v1_user
-      render json: @user.wishes, status: 200
+      @user      = current_v1_user
+      categories = params[:categories].try(:split, '-').try(:map, &:to_i)
+
+      @wishes    = @user.wishes
+      if categories
+        ids      = current_v1_user.wishes.map(&:wisheable).pluck(:id)
+        products = Product.where(id: ids)
+                          .ransack(categories_id_in: categories)
+                          .result
+        @wishes  = current_v1_user.wishes.where(id: products.pluck(:id))
+      end
+
+      render json: @wishes, status: 200
     end
 
     def wishes_by_user
