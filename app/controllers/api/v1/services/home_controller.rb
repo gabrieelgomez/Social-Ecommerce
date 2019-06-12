c888a72e77fe2f856136823c495162cb12b775e0
gabrielgomez <gagg1707@gmail.com>
2019-06-11 20:48:34 -0400

    refactor /home controller admit filter countries

--- app/controllers/api/v1/services/home_controller.rb
+++ app/controllers/api/v1/services/home_controller.rb
@@ -8,14 +8,16 @@ module Api::V1::Services
     end
 
     def build_json(params)
+      states        = params[:states_codes].try(:split, '-').try(:map, &:to_s)
+      countries     = params[:countries_codes].try(:split, '-').try(:map, &:to_s)
       @data = {
         data: {
-          profiles_by_created_products: Home::HomeService.profiles(params),
-          independents_by_created_at: Home::HomeService.independents(params),
-          sellers_by_followers: Home::HomeService.sellers(params),
+          profiles_by_created_products: Home::HomeService.profiles(params, states, countries),
+          independents_by_created_at: Home::HomeService.independents(params, states, countries),
+          sellers_by_followers: Home::HomeService.sellers(params, states, countries),
           categories_listing_by_products: Home::HomeService.categories,
-          products_by_created_at: Home::HomeService.products_by_created_at(params),
-          products_by_wishes: Home::HomeService.products_by_wishes(params),
+          products_by_created_at: Home::HomeService.products_by_created_at(params, states, countries),
+          products_by_wishes: Home::HomeService.products_by_wishes(params, states, countries),
           categories_by_profiles_nested_products: Home::HomeService.categories_by_profiles_nested_products
         }
       }
--- app/services/home/home_service.rb
+++ app/services/home/home_service.rb
@@ -2,46 +2,50 @@ module Home
 
   class HomeService
     # Services #2 independents_by_created_at, Enmachs
-    def self.independents(params)
+    def self.independents(params, states, countries)
       quantity = params[:quantity].to_i || 10
-      Independent.order(created_at: :desc)
+      profiles = Independent.order(created_at: :desc)
                  .reject{|i| i.photo.blank?}
                  .first(quantity)
-                 .as_json(only: %i[id title slug photo type_profile created_at], methods: %i[category_ids follow])
+      profiles = Profile.search_by_countries_states(profiles, states, countries) if countries || states
+      profiles.as_json(only: %i[id title slug photo type_profile created_at], methods: %i[category_ids follow])
     end
 
     # Services #3 sellers_by_followers, Enmachs
-    def self.sellers(params)
+    def self.sellers(params, states, countries)
       quantity = params[:quantity].to_i || 10
-      Seller.all
+      profiles = Seller.all
             .reject{|i| i.photo.blank?}
             .sort_by{ |seller| seller.followers_by_type_profile('User', 'Seller').count }
             .reverse
             .first(quantity)
-            .as_json(only: %i[id title slug photo type_profile created_at], methods: %i[category_ids follow])
+      profiles = Profile.search_by_countries_states(profiles, states, countries) if countries || states
+      profiles.as_json(only: %i[id title slug photo type_profile created_at], methods: %i[category_ids follow])
     end
 
     # Services #5 products_by_created_at, Enmachs
-    def self.products_by_created_at(params)
+    def self.products_by_created_at(params, states, countries)
       quantity = params[:quantity].to_i || 10
-      Product.order(created_at: :desc)
+      products = Product.order(created_at: :desc)
              .reject{|i| i.cover.url.blank?}
              .first(quantity)
-             .as_json(
+      products = Product.search_by_countries_states(products, states, countries) if countries || states
+      products.as_json(
                only: %i[id cover name price created_at],
                methods: %i[owner wish links url_get]
              )
     end
 
     # Services #6 products_by_wishes, Enmachs
-    def self.products_by_wishes(params)
+    def self.products_by_wishes(params, states, countries)
       quantity = params[:quantity].to_i || 10
-      Product.all
+      products = Product.all
              .reject{|i| i.cover.url.blank?}
              .sort_by{ |product| product.wishes.count }
              .reverse
              .first(quantity)
-             .as_json(only: %i[id cover name price created_at],
+      products = Product.search_by_countries_states(products, states, countries) if countries || states
+      products.as_json(only: %i[id cover name price created_at],
                methods: %i[owner wish links url_get]
              )
     end
@@ -49,14 +53,15 @@ module Home
     # --------------------------------------------------------------------------
 
     # Services #1 profiles_by_created_products, GabrielGomez
-    def self.profiles(params)
+    def self.profiles(params, states, countries)
       quantity = params[:quantity].to_i || 10
-      Pyme.all
+      profiles = Pyme.all
           .reject{|i| i.photo.blank?}
           .sort_by { |profile| (profile.products.map(&:status)-[false]).count }
           .reverse
           .first(quantity)
-          .as_json(only: %i[id title slug photo type_profile], methods: %i[category_ids follow])
+      profiles = Profile.search_by_countries_states(profiles, states, countries) if countries || states
+      profiles.as_json(only: %i[id title slug photo type_profile], methods: %i[category_ids follow])
     end
 
     # Services #4 categories_listing_by_products, GabrielGomez
