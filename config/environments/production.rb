8cfdc564e981a89aa9464dbc0649a466e7c89ccb
gabrieelgomez <gagg1707@gmail.com>
2019-10-02 19:18:52 -0400

    change http to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.web_socket_server_url = "http://api.ibigwave.com/cable"
+  config.web_socket_server_url = "https://api.ibigwave.com/cable"
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   # config.action_cable.allowed_request_origins = [ 'http://api.ibigwave.com']
