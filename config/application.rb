0c7ed4c9a18e52ed54bf7ab8129e2fc5ac5faf03
gabrieelgomez <gagg1707@gmail.com>
2019-10-05 16:46:40 -0400

    remove cookies

--- config/application.rb
+++ config/application.rb
@@ -38,9 +38,9 @@ module BigwaveApi
     config.middleware.use ActionDispatch::Flash
 
 
-    config.session_store :cookie_store, key: '_interslice_session'
-    config.middleware.use ActionDispatch::Cookies # Required for all session management
-    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    # config.session_store :cookie_store, key: '_interslice_session'
+    # config.middleware.use ActionDispatch::Cookies # Required for all session management
+    # config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
 
     # config.middleware.insert_before 0, Rack::Cors do
     #   allow do
